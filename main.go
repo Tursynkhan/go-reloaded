0e3998730af83b492c25c8ecbbbb1ba09789c745
tursynkhan <student@ALEM-7dbc2a.alem.school>
2022-06-12 19:56:56 +0600

    initial commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,9 +16,23 @@ func main() {
 	}
 	defer file1.Close()
 
+	var str string
 	scanner := bufio.NewScanner(file1)
 	for scanner.Scan() {
-		str := (scanner.Text())
-		fmt.Println(str)
+		str += scanner.Text()
 	}
+
+	Up(&str)
+}
+
+func Up(str *string) {
+	word := strings.Fields(*str)
+	for i := 0; i < len(word); i++ {
+		if word[i] == "(up)" {
+			word[i-1] = strings.ToUpper(word[i-1])
+			word = append(word[:i], word[i+1:]...)
+		}
+	}
+	fmt.Println(word)
+	*str = strings.Join(word, " ")
 }
